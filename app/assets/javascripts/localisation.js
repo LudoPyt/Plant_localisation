e8373b6f0bc97482deb9fc3c29bb1ee333355481
leo mouraire <l.mouraire@outlook.fr>
2019-02-20 19:45:06 +0100

    correction localisation

--- app/assets/javascripts/localisation.js
+++ app/assets/javascripts/localisation.js
@@ -5,39 +5,41 @@ window.onload = function() {
   var alertLoc = document.getElementById("alert-localisation");
 
   btn.addEventListener("click", getLocation);
-}
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-    // navigator.geolocation.watchPosition(showPosition, showError);
-  } else {
-    alertLoc.innerHTML = "Geolocation is not supported by this browser.";
+  console.log(document.getElementById("latitude"));
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+      // navigator.geolocation.watchPosition(showPosition, showError);
+    } else {
+      alertLoc.innerHTML = "Geolocation is not supported by this browser.";
+    }
   }
-}
 
-function showPosition(position) {
-  console.log('position');
-  lat.value = position.coords.latitude;
-  long.value = position.coords.longitude;
-  lat.disabled = true;
-  long.disabled = true;
-}
+  function showPosition(position) {
+    console.log('position');
+    lat.value = position.coords.latitude;
+    long.value = position.coords.longitude;
+    lat.disabled = true;
+    long.disabled = true;
+  }
 
-function showError(error) {
-  console.log('error');
-  switch(error.code) {
-    case error.PERMISSION_DENIED:
-      alertLoc.innerHTML = "User denied the request for Geolocation."
-      break;
-    case error.POSITION_UNAVAILABLE:
-      alertLoc.innerHTML = "Location information is unavailable."
-      break;
-    case error.TIMEOUT:
-      alertLoc.innerHTML = "The request to get user location timed out."
-      break;
-    case error.UNKNOWN_ERROR:
-      alertLoc.innerHTML = "An unknown error occurred."
-      break;
+  function showError(error) {
+    console.log('error');
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        alertLoc.innerHTML = "User denied the request for Geolocation."
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alertLoc.innerHTML = "Location information is unavailable."
+        break;
+      case error.TIMEOUT:
+        alertLoc.innerHTML = "The request to get user location timed out."
+        break;
+      case error.UNKNOWN_ERROR:
+        alertLoc.innerHTML = "An unknown error occurred."
+        break;
+    }
   }
 }
