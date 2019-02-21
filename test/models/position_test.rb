93a63e7920411ef321f8a495a0dea972bf6bc5ec
BastienBah <bastien.bahuet0704@gmail.com>
2019-02-21 11:19:15 +0100

    adding csv datas in rake task file

--- test/models/position_test.rb
+++ test/models/position_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: positions
+#
+#  id         :bigint(8)        not null, primary key
+#  latitude   :float
+#  longitude  :float
+#  plant_id   :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PositionTest < ActiveSupport::TestCase
