406097d853b9c1d8ca3586213c2266c53674bac4
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-03-12 17:09:14 +0100

    delete rose route

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'positions#index'
-  get '/roses' => 'plants#index', as: :roses
-  get '/roses/:id' => 'plants#show', as: :rose
   get '/users' => 'users#index'
   get '/users/:id/approve' => 'users#approve', as: :approve_user
 end
